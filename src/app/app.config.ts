0d73e47c679d416aee0cd1e9789337ff432eecff
Kevin Müller <kevin.mueller@tuta.com>
2024-05-15 18:19:01 +0200

    added firestore service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,11 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { Environment } from './environment';
+
+
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), 
@@ -13,7 +18,7 @@ export const appConfig: ApplicationConfig = {
       "projectId":"simple-crm-dfd4a",
       "appId":"1:458563173555:web:f2093ee933803f496e04c6",
       "storageBucket":"simple-crm-dfd4a.appspot.com",
-      "apiKey":"AIzaSyAp6mYF4R7dpN5KIulJO-K8Hu7cmKUg-U0",
+      "apiKey":Environment.API_KEY,
       "authDomain":"simple-crm-dfd4a.firebaseapp.com",
       "messagingSenderId":"458563173555"})), 
       provideFirestore(() => getFirestore())]
